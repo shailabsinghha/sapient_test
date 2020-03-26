26be55b6d20eb999fe943b993320512c58d4bdaa
Saumya <saumya@Saumyas-MacBook-Air.local>
2020-03-27 01:23:39 +0530

    make it better

---
+++ src/main/java/com/sapient/al2/Al2Application.java
@@ -0,0 +1,13 @@
+package com.sapient.al2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Al2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Al2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sapient/al2/Al2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.al2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Al2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
